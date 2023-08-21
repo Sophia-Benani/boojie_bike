5711d411af0993982c1aa2d73a2269ef310b1913
Sophia Benani <sophiabenani@gmail.com>
2023-08-21 17:45:38 +0200

    seed done

---
+++ db/migrate/20230821152819_modify_type_to_bikes.rb
@@ -0,0 +1,5 @@
+class ModifyTypeToBikes < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :bikes, :type, :category
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Cleaning database..."
+Bike.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+elodie = User.create!({ first_name: "Elodie", last_name:"Faure", email: "efaure@bla.com", password: "azerty" })
+david = User.create!({ first_name: "David", last_name:"Berchier", email: "david-b@bla.com", password: "azerty" })
+
+
+puts "Creating bikes..."
+electro = {name: "Electro", brand:"O2feel", address: "Bordeaux", category: "électrique", description: "on l'appelle speedy", accessories: "panier", user_id: elodie.id}
+bolide =  {name: "Bolide", brand:"Peugeot", address: "Paris", category: "VTT", description: "géniale en forêt", accessories: "anti-vol", user_id: david.id }
+
+[electro, bolide].each do |attributes|
+  bike = Bike.create!(attributes)
+  puts "Created #{bike.name}"
+end
+puts "Finished!"
