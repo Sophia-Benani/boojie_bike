7c55135537fed9b7602cc4f7acb1beb0cf3fb50b
elodie faure <efaure33@wanadoo.fr>
2023-08-22 13:05:22 +0200

    ajout de prix au bike

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_152819) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_104234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_152819) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
