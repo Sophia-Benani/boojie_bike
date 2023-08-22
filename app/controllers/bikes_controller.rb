5d85e93e16507efef8f0eaa39cddaef3cfd7f3c3
elodie faure <efaure33@wanadoo.fr>
2023-08-22 17:30:54 +0200

    alors : 1 formulaire new bike 2: active storage + cloudinary 3: photo sur bike show et index

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -8,4 +8,23 @@ class BikesController < ApplicationController
     renter = @bike.user_id
     @user = User.find(renter)
   end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+    @bike.user_id = current_user.id
+    if @bike.save
+      redirect_to bikes_path, notice: 'Bike was successfully created.'
+    end
+  end
+
+end
+
+private
+
+def bike_params
+  params.require(:bike).permit(:name, :brand, :category, :description, :address, :accessories, :price, photos: [])
 end
